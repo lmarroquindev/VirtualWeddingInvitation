c03a1a3befa85199419d23ccd8b2f33df83c1a06
lmarroquinarkusnexus <lmarroquin@arkusnexus.com>
2025-10-14 23:32:14 -0600

    Se programó sección de preguntas y respuestas, se reorganizómel contenido en index html y se bloqueo boton de subir fotos

--- js/apiConfig.js
+++ js/apiConfig.js
@@ -3,5 +3,5 @@
 // Configuración general de la API
 // ========================================================
 
-const API_BASE_URL = "https://localhost:7044/api";
-// const API_BASE_URL = "https://bodamarroquinrodriguezapi.runasp.net/api";
+// const API_BASE_URL = "https://localhost:7044/api";
+const API_BASE_URL = "https://bodamarroquinrodriguezapi.runasp.net/api";
